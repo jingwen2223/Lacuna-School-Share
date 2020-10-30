6122c450f21b87931fce4d149a5ab6a9dd4c9fa1
Edison Blum <blum.edison12@gmail.com>
2020-10-30 10:58:54 -0400

    Update App.js
    making new branch

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="alert alert-info" >
-          School Supplies
+        <div className="alert alert-info">
+          School Supplie
           <hr />
           <div className="container">
             <div className="col-md-14">
-            <Bag />
+              <Bag />
               <hr />
               <Sort_Items />
               <hr />
@@ -28,7 +28,6 @@ class App extends Component {
           </div>
         </div>
       </Provider>
-      
     );
   }
 }
